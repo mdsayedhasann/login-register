2c4503391c411d49c726b16695cdf475cfb5462c
sayed_hossain <sayed.hasan0011@gmail.com>
2023-09-30 19:19:08 +0600

    A Project Using React Router, Tailwind, Daisy Ui, setup the Root, make header

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./component/Root/Root.jsx";
+import Home from "./component/Home/Home.jsx";
+import Register from "./component/Register/Register.jsx";
+import Login from "./component/Login/Login.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
